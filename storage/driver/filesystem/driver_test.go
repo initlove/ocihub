2dbbc00184fb07519a988d77dec2eaa9485c034b
David Liang <liangchenye@gmail.com>
2017-08-10 20:19:27 +0800

    add driver
    Signed-off-by: David Liang <liangchenye@gmail.com>

---
+++ storage/driver/filesystem/driver_test.go
@@ -0,0 +1,29 @@
+package filesystem
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/astaxie/beego/context"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPut(t *testing.T) {
+	tmpDir, _ := ioutil.TempDir("", "ocidata")
+	defer os.RemoveAll(tmpDir)
+
+	paras := make(map[string]interface{})
+	paras["rootDirectory"] = tmpDir
+
+	var ctx context.Context
+	var d driver
+	d.Create(paras)
+
+	testPath := "testPath"
+	testData := []byte("testdata")
+
+	d.PutContent(ctx, testPath, testData)
+	data, _ := d.GetContent(ctx, testPath)
+	assert.Equal(t, testData, data)
+}
