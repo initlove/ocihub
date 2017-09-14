02e5558fb35fc5e1cf864983673418a9498f9dd6
Liang Chenye <liangchenye@huawei.com>
2017-09-14 15:24:33 +0800

    add ignore ut
    Signed-off-by: Liang Chenye <liangchenye@huawei.com>

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+ignore=(
+# most of this code are copied from docker/distribution, so don't re-work on the ut for now.
+"github.com/initlove/ocihub/storage/driver/filesystem"
+)
 
 set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
+	ret=0
+	for i in $ignore
+	do 
+		if [ $i == $d ]
+		then
+			ret=1
+			break
+		fi
+	done
+	if [ $ret == 1 ]
+	then
+		break
+	fi
 	go test -race -coverprofile=profile.out -covermode=atomic $d
  	if [ -f profile.out ]; then
 		cat profile.out >> coverage.txt
